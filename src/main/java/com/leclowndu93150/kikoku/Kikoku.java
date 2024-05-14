33a659a51abedbf7549d4e2a5af055f9340ed823
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-05-14 23:40:30 +0200

    Shit

--- src/main/java/com/leclowndu93150/kikoku/Kikoku.java
+++ src/main/java/com/leclowndu93150/kikoku/Kikoku.java
@@ -1,19 +1,15 @@
 package com.leclowndu93150.kikoku;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
-
-import java.util.function.Supplier;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Kikoku.MODID)
